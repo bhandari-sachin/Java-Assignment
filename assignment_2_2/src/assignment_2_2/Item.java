406f0978b424b9c55299d91d769c9e764bfff79b
sachin bhandari <bhandari.sachin.fi@gmail.com>
2025-09-02 02:51:57 +0300

    Helper class for task 3 & 4

--- assignment_2_2/src/assignment_2_2/Item.java
+++ assignment_2_2/src/assignment_2_2/Item.java
@@ -1,18 +1,33 @@
+
+// This Item class is to represent each grocery item with multiple attributes
+
 package assignment_2_2;
 
 class Item {
     String name;
     double cost;
     String category;
+    int quantity;
 
-    Item(String name, double cost, String category) {
+    Item(String name, double cost, String category, int quantity) {
         this.name = name;
         this.cost = cost;
         this.category = category;
+        this.quantity = quantity;
     }
 
+    Item(String name, double cost, String category) {
+        this(name, cost, category,1);
+    }
+
+
+    @Override
     public String toString() {
-        return name + " : €" + cost + " (" + category + ")";
+        if (quantity > 1) {
+            return name + " : €" + cost + " (" + category + ") x" + quantity;
+        } else {
+            return name + " : €" + cost + " (" + category + ")";
+        }
     }
-}
 
+}
